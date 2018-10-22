9276ac6b6a19e09289474653d6851f7918767b26
Federico Tomasi <fdtomasi@gmail.com>
2018-10-22 16:42:47 +0200

    refactoring to allow for setup.py develop

--- mtmkl/utils.py
+++ mtmkl/utils.py
@@ -1,21 +1,24 @@
 """Utility functions for the mt-mkl."""
-import numpy as np
 import collections
+
+import numpy as np
+
 
 def flatten(lst):
     """Flatten a list."""
     return [y for l in lst for y in flatten(l)] \
         if isinstance(lst, (list, np.ndarray)) else [lst]
-        
-        
+
+
 def generate_index(X_list, y_list, cv):
     X_list_transpose = [X.transpose(1, 2, 0) for X in X_list]
     split = [cv.split(X, y) for X, y in zip(X_list_transpose, y_list)]
-    n_splits = min(cv.get_n_splits(X, y, None) for X, y in zip(X_list_transpose, y_list))
+    n_splits = min(
+        cv.get_n_splits(X, y, None) for X, y in zip(X_list_transpose, y_list))
 
-    for i in range(n_splits):
+    for _ in range(n_splits):
         yield zip(*[next(s) for s in split])
-        
+
 
 def update_rho(rho, rnorm, snorm, iteration=None, mu=10, tau_inc=2, tau_dec=2):
     """See Boyd pag 20-21 for details.
@@ -23,14 +26,15 @@ def update_rho(rho, rnorm, snorm, iteration=None, mu=10, tau_inc=2, tau_dec=2):
     Parameters
     ----------
     rho : float
+
     """
     if rnorm > mu * snorm:
         return tau_inc * rho
     elif snorm > mu * rnorm:
         return rho / tau_dec
     return rho
-    
-    
+
+
 def namedtuple_with_defaults(typename, field_names, default_values=()):
     T = collections.namedtuple(typename, field_names)
     T.__new__.__defaults__ = (None, ) * len(T._fields)
