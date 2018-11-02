b3913664846f5bafd3ee442390adc71b01a3ce77
Unknown <vanedamario@gmail.com>
2018-11-02 18:20:36 +0100

    change balanced_accuracy

--- mtmkl/metrics.py
+++ mtmkl/metrics.py
@@ -1,7 +1,8 @@
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import balanced_accuracy_score
-    
-    
+
+
 def balanced_accuracy_multiple(y_true, y_pred, sample_weight=None):
     ba = [balanced_accuracy_score(yt, yp) for yt, yp in zip(y_true, y_pred)]
     #for p, b in zip(PATIENTS, ba):
