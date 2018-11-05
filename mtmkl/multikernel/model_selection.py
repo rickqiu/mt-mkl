be30c24ea55cef2e136f97468298d5d6483964a3
vanessa <vanedamario@gmail.com>
2018-11-05 10:10:13 +0100

    Multiple gridsearch is now a superclass

--- mtmkl/multikernel/model_selection.py
+++ mtmkl/multikernel/model_selection.py
@@ -23,6 +23,8 @@ from sklearn.utils.fixes import MaskedArray
 from sklearn.utils.metaestimators import _safe_split
 from sklearn.utils.validation import _num_samples
 
+from mtmkl.utils import generate_index
+
 
 def _safe_split_multi(estimator, X, y, train, test):
     X_train, y_train, X_test, y_test = [], [], [], []
@@ -388,18 +390,7 @@ def cross_validate(
     cv = check_cv(cv, y, classifier=is_classifier(estimator))
     scorers, _ = _check_multimetric_scoring(estimator, scoring=scoring)
 
-    n_splits = min(
-        cv.get_n_splits(X_.transpose(1, 2, 0), y_, None)
-        for X_, y_ in zip(X, y))
-
-    def generate_index(X_list, y_list):
-        split = [
-            cv.split(X.transpose(1, 2, 0), y) for X, y in zip(X_list, y_list)
-        ]
-        for i in range(n_splits):
-            yield zip(*[next(s) for s in split])
-
-    generate_index_iter = generate_index(X, y)
+    generate_index_iter = generate_index(X, y, cv)
 
     # We clone the estimator to make sure that all the folds are
     # independent, and that it is pickle-able.
@@ -527,15 +518,7 @@ class MultipleKernelGridSearchCV(GridSearchCV):
             cv.get_n_splits(X_.transpose(1, 2, 0), y_, None)
             for X_, y_ in zip(X, y))
 
-        def generate_index(X_list, y_list):
-            split = [
-                cv.split(X.transpose(1, 2, 0), y)
-                for X, y in zip(X_list, y_list)
-            ]
-            for i in range(n_splits):
-                yield zip(*[next(s) for s in split])
-
-        generate_index_iter = generate_index(X, y)
+        generate_index_iter = generate_index(X, y, cv)
 
         # Regenerate parameter iterable for each fit
         candidate_params = list(self._get_param_iterator())
@@ -684,7 +667,8 @@ class MultipleKernelGridSearchCV(GridSearchCV):
         return self
 
 
-class MultipleKernelRandomizedSearchCV(RandomizedSearchCV):
+class MultipleKernelRandomizedSearchCV(RandomizedSearchCV,
+                                       MultipleKernelGridSearchCV):
     # Ensure consistent split
     _pairwise = True
 
@@ -713,206 +697,5 @@ class MultipleKernelRandomizedSearchCV(RandomizedSearchCV):
             Parameters passed to the ``fit`` method of the estimator
 
         """
-        if self.fit_params is not None:
-            warnings.warn(
-                '"fit_params" as a constructor argument was '
-                'deprecated in version 0.19 and will be removed '
-                'in version 0.21. Pass fit parameters to the '
-                '"fit" method instead.', DeprecationWarning)
-            if fit_params:
-                warnings.warn(
-                    'Ignoring fit_params passed as a constructor '
-                    'argument in favor of keyword arguments to '
-                    'the "fit" method.', RuntimeWarning)
-            else:
-                fit_params = self.fit_params
-        estimator = self.estimator
-        cv = check_cv(self.cv, y, classifier=is_classifier(estimator))
-
-        scorers, self.multimetric_ = _check_multimetric_scoring(
-            self.estimator, scoring=self.scoring)
-
-        if self.multimetric_:
-            if self.refit is not False and (
-                    not isinstance(self.refit, six.string_types)
-                    or  # This will work for both dict / list (tuple)
-                    self.refit not in scorers):
-                raise ValueError(
-                    "For multi-metric scoring, the parameter "
-                    "refit must be set to a scorer key "
-                    "to refit an estimator with the best "
-                    "parameter setting on the whole data and "
-                    "make the best_* attributes "
-                    "available for that metric. If this is not "
-                    "needed, refit should be set to False "
-                    "explicitly. %r was passed." % self.refit)
-            else:
-                refit_metric = self.refit
-        else:
-            refit_metric = 'score'
-
-        # X, y, groups = indexable(X, y, groups)
-        if groups is not None:
-            raise NotImplementedError("groups are not supported")
-
-        # n_splits = cv.get_n_splits(X, y, groups)
-        n_splits = min(
-            cv.get_n_splits(X_.transpose(1, 2, 0), y_, None)
-            for X_, y_ in zip(X, y))
-
-        def generate_index(X_list, y_list):
-            split = [
-                cv.split(X.transpose(1, 2, 0), y)
-                for X, y in zip(X_list, y_list)
-            ]
-            for i in range(n_splits):
-                yield zip(*[next(s) for s in split])
-
-        generate_index_iter = generate_index(X, y)
-
-        # Regenerate parameter iterable for each fit
-        candidate_params = list(self._get_param_iterator())
-        n_candidates = len(candidate_params)
-        if self.verbose > 0:
-            print(
-                "Fitting {0} folds for each of {1} candidates, totalling"
-                " {2} fits".format(
-                    n_splits, n_candidates, n_candidates * n_splits))
-
-        base_estimator = clone(self.estimator)
-        pre_dispatch = self.pre_dispatch
-
-        out = Parallel(
-            n_jobs=self.n_jobs, verbose=self.verbose,
-            pre_dispatch=pre_dispatch)(
-                delayed(_fit_and_score)(
-                    clone(base_estimator), X, y, scorers, train, test, self.
-                    verbose, parameters, fit_params=fit_params,
-                    return_train_score=self.return_train_score,
-                    return_n_test_samples=True, return_times=True,
-                    return_parameters=False, error_score=self.error_score,
-                    return_estimator=True, return_idx=True)
-                for parameters, (
-                    train,
-                    test) in product(candidate_params, generate_index_iter))
-
-        # if one choose to see train score, "out" will contain train score info
-        if self.return_train_score:
-            (
-                train_score_dicts, test_score_dicts, test_sample_counts,
-                fit_time, score_time, estimators, train_idxs,
-                test_idxs) = zip(*out)
-        else:
-            (
-                test_score_dicts, test_sample_counts, fit_time, score_time,
-                estimators, train_idxs, test_idxs) = zip(*out)
-
-        # test_score_dicts and train_score dicts are lists of dictionaries and
-        # we make them into dict of lists
-        test_scores = _aggregate_score_dicts(test_score_dicts)
-        if self.return_train_score:
-            train_scores = _aggregate_score_dicts(train_score_dicts)
-
-        # TODO: replace by a dict in 0.21
-        results = (
-            DeprecationDict() if self.return_train_score == 'warn' else {})
-
-        # results = {}
-
-        def _store(key_name, array, weights=None, splits=False, rank=False):
-            """Store the scores/times to the cv_results_."""
-            # When iterated first by splits, then by parameters
-            # We want `array` to have `n_candidates` rows and `n_splits` cols.
-            array = np.array(array, dtype=np.float64).reshape(
-                n_candidates, n_splits)
-            if splits:
-                for split_i in range(n_splits):
-                    # Uses closure to alter the results
-                    results["split%d_%s" % (split_i,
-                                            key_name)] = array[:, split_i]
-
-            array_means = np.average(array, axis=1, weights=weights)
-            results['mean_%s' % key_name] = array_means
-            # Weighted std is not directly available in numpy
-            array_stds = np.sqrt(
-                np.average(
-                    (array - array_means[:, np.newaxis])**2, axis=1,
-                    weights=weights))
-            results['std_%s' % key_name] = array_stds
-
-            if rank:
-                results["rank_%s" % key_name] = np.asarray(
-                    rankdata(-array_means, method='min'), dtype=np.int32)
-
-        _store('fit_time', fit_time)
-        _store('score_time', score_time)
-        results['estimators'] = estimators
-        results['train_index'] = train_idxs
-        results['test_index'] = test_idxs
-
-        # Use one MaskedArray and mask all the places where the param is not
-        # applicable for that candidate. Use defaultdict as each candidate may
-        # not contain all the params
-        param_results = defaultdict(
-            partial(
-                MaskedArray, np.empty(n_candidates, ), mask=True,
-                dtype=object))
-        for cand_i, params in enumerate(candidate_params):
-            for name, value in params.items():
-                # An all masked empty array gets created for the key
-                # `"param_%s" % name` at the first occurence of `name`.
-                # Setting the value at an index also unmasks that index
-                param_results["param_%s" % name][cand_i] = value
-
-        results.update(param_results)
-        # Store a list of param dicts at the key 'params'
-        results['params'] = candidate_params
-
-        # NOTE test_sample counts (weights) remain the same for all candidates
-        test_sample_counts = np.array(
-            test_sample_counts[:n_splits], dtype=np.int)
-        for scorer_name in scorers.keys():
-            # Computed the (weighted) mean and std for test scores alone
-            _store(
-                'test_%s' % scorer_name, test_scores[scorer_name], splits=True,
-                rank=True, weights=test_sample_counts if self.iid else None)
-            if self.return_train_score:
-                prev_keys = set(results.keys())
-                _store(
-                    'train_%s' % scorer_name, train_scores[scorer_name],
-                    splits=True)
-
-                if self.return_train_score == 'warn':
-                    for key in set(results.keys()) - prev_keys:
-                        message = (
-                            'You are accessing a training score ({!r}), '
-                            'which will not be available by default '
-                            'any more in 0.21. If you need training scores, '
-                            'please set return_train_score=True').format(key)
-                        # warn on key access
-                        results.add_warning(key, message, FutureWarning)
-
-        # For multi-metric evaluation, store the best_index_, best_params_ and
-        # best_score_ iff refit is one of the scorer names
-        # In single metric evaluation, refit_metric is "score"
-        if self.refit or not self.multimetric_:
-            self.best_index_ = results["rank_test_%s" % refit_metric].argmin()
-            self.best_params_ = candidate_params[self.best_index_]
-            self.best_score_ = results["mean_test_%s" %
-                                       refit_metric][self.best_index_]
-
-        if self.refit:
-            self.best_estimator_ = clone(base_estimator).set_params(
-                **self.best_params_)
-            if y is not None:
-                self.best_estimator_.fit(X, y, **fit_params)
-            else:
-                self.best_estimator_.fit(X, **fit_params)
-
-        # Store the only scorer not as a dict for single metric evaluation
-        self.scorer_ = scorers if self.multimetric_ else scorers['score']
-
-        self.cv_results_ = results
-        self.n_splits_ = n_splits
-
-        return self
+        return MultipleKernelGridSearchCV.fit(
+            self, X=X, y=y, groups=groups, **fit_params)
