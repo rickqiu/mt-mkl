de8d07508721b972999963532b6421ce4fd54cf4
Federico Tomasi <fdtomasi@gmail.com>
2018-11-07 09:10:51 +0100

    change order functions

--- mtmkl/multikernel/model_selection.py
+++ mtmkl/multikernel/model_selection.py
@@ -43,189 +43,6 @@ def _safe_split_multi(estimator, X, y, train, test):
     return X_train, y_train, X_test, y_test
 
 
-def _fit_and_score(
-        estimator, X, y, scorer, train, test, verbose, parameters, fit_params,
-        return_train_score=False, return_parameters=False,
-        return_n_test_samples=False, return_times=False, error_score='raise',
-        return_estimator=False, return_idx=True):
-    """Fit estimator and compute scores for a given dataset split.
-
-    Parameters
-    ----------
-    estimator : estimator object implementing 'fit'
-        The object to use to fit the data.
-
-    X : array-like of shape at least 2D
-        The data to fit.
-
-    y : array-like, optional, default: None
-        The target variable to try to predict in the case of
-        supervised learning.
-
-    scorer : A single callable or dict mapping scorer name to the callable
-        If it is a single callable, the return value for ``train_scores`` and
-        ``test_scores`` is a single float.
-
-        For a dict, it should be one mapping the scorer name to the scorer
-        callable object / function.
-
-        The callable object / fn should have signature
-        ``scorer(estimator, X, y)``.
-
-    train : array-like, shape (n_train_samples,)
-        Indices of training samples.
-
-    test : array-like, shape (n_test_samples,)
-        Indices of test samples.
-
-    verbose : integer
-        The verbosity level.
-
-    error_score : 'raise' (default) or numeric
-        Value to assign to the score if an error occurs in estimator fitting.
-        If set to 'raise', the error is raised. If a numeric value is given,
-        FitFailedWarning is raised. This parameter does not affect the refit
-        step, which will always raise the error.
-
-    parameters : dict or None
-        Parameters to be set on the estimator.
-
-    fit_params : dict or None
-        Parameters that will be passed to ``estimator.fit``.
-
-    return_train_score : boolean, optional, default: False
-        Compute and return score on training set.
-
-    return_parameters : boolean, optional, default: False
-        Return parameters that has been used for the estimator.
-
-    return_n_test_samples : boolean, optional, default: False
-        Whether to return the ``n_test_samples``
-
-    return_times : boolean, optional, default: False
-        Whether to return the fit/score times.
-
-    Returns
-    -------
-    train_scores : dict of scorer name -> float, optional
-        Score on training set (for all the scorers),
-        returned only if `return_train_score` is `True`.
-
-    test_scores : dict of scorer name -> float, optional
-        Score on testing set (for all the scorers).
-
-    n_test_samples : int
-        Number of test samples.
-
-    fit_time : float
-        Time spent for fitting in seconds.
-
-    score_time : float
-        Time spent for scoring in seconds.
-
-    parameters : dict or None, optional
-        The parameters that have been evaluated.
-
-    """
-    if verbose > 1:
-        if parameters is None:
-            msg = ''
-        else:
-            msg = '%s' % (
-                ', '.join('%s=%s' % (k, v) for k, v in parameters.items()))
-        print("[CV] %s %s" % (msg, (64 - len(msg)) * '.'))
-
-    # Adjust length of sample weights
-    fit_params = fit_params if fit_params is not None else {}
-    fit_params = dict(
-        [(k, _index_param_value(X, v, train)) for k, v in fit_params.items()])
-
-    test_scores = {}
-    train_scores = {}
-    if parameters is not None:
-        estimator.set_params(**parameters)
-
-    start_time = time.time()
-
-    # do it for each patient
-    X_train, y_train, X_test, y_test = _safe_split_multi(
-        estimator, X, y, train, test)
-
-    is_multimetric = not callable(scorer)
-    n_scorers = len(scorer.keys()) if is_multimetric else 1
-
-    try:
-        if y_train is None:
-            estimator.fit(X_train, **fit_params)
-        else:
-            estimator.fit(X_train, y_train, **fit_params)
-
-    except Exception as e:
-        # Note fit time as time until error
-        fit_time = time.time() - start_time
-        score_time = 0.0
-        if error_score == 'raise':
-            raise
-        elif isinstance(error_score, numbers.Number):
-            if is_multimetric:
-                test_scores = dict(
-                    zip(scorer.keys(), [
-                        error_score,
-                    ] * n_scorers))
-                if return_train_score:
-                    train_scores = dict(
-                        zip(scorer.keys(), [
-                            error_score,
-                        ] * n_scorers))
-            else:
-                test_scores = error_score
-                if return_train_score:
-                    train_scores = error_score
-            warnings.warn(
-                "Classifier fit failed. The score on this train-test"
-                " partition for these parameters will be set to %f. "
-                "Details: \n%r" % (error_score, e), FitFailedWarning)
-        else:
-            raise ValueError(
-                "error_score must be the string 'raise' or a"
-                " numeric value. (Hint: if using 'raise', please"
-                " make sure that it has been spelled correctly.)")
-
-    else:
-        fit_time = time.time() - start_time
-        # _score will return dict if is_multimetric is True
-        test_scores = _score(estimator, X_test, y_test, scorer, is_multimetric)
-        score_time = time.time() - start_time - fit_time
-        if return_train_score:
-            train_scores = _score(
-                estimator, X_train, y_train, scorer, is_multimetric)
-
-    if verbose > 2:
-        if is_multimetric:
-            for scorer_name, score in test_scores.items():
-                msg += ", %s=%s" % (scorer_name, score)
-        else:
-            msg += ", score=%s" % test_scores
-    if verbose > 1:
-        total_time = score_time + fit_time
-        end_msg = "%s, total=%s" % (msg, logger.short_format_time(total_time))
-        print("[CV] %s %s" % ((64 - len(end_msg)) * '.', end_msg))
-
-    ret = [train_scores, test_scores] if return_train_score else [test_scores]
-
-    if return_n_test_samples:
-        ret.append(_num_samples(X_test))
-    if return_times:
-        ret.extend([fit_time, score_time])
-    if return_parameters:
-        ret.append(parameters)
-    if return_estimator:
-        ret.append(estimator)
-    if return_idx:
-        ret.extend([train, test])
-    return ret
-
-
 def cross_validate(
         estimator, X, y=None, groups=None, scoring=None, cv=None, n_jobs=1,
         verbose=0, fit_params=None, pre_dispatch='2*n_jobs',
@@ -444,6 +261,189 @@ def cross_validate(
     return ret
 
 
+def _fit_and_score(
+        estimator, X, y, scorer, train, test, verbose, parameters, fit_params,
+        return_train_score=False, return_parameters=False,
+        return_n_test_samples=False, return_times=False, error_score='raise',
+        return_estimator=False, return_idx=True):
+    """Fit estimator and compute scores for a given dataset split.
+
+    Parameters
+    ----------
+    estimator : estimator object implementing 'fit'
+        The object to use to fit the data.
+
+    X : array-like of shape at least 2D
+        The data to fit.
+
+    y : array-like, optional, default: None
+        The target variable to try to predict in the case of
+        supervised learning.
+
+    scorer : A single callable or dict mapping scorer name to the callable
+        If it is a single callable, the return value for ``train_scores`` and
+        ``test_scores`` is a single float.
+
+        For a dict, it should be one mapping the scorer name to the scorer
+        callable object / function.
+
+        The callable object / fn should have signature
+        ``scorer(estimator, X, y)``.
+
+    train : array-like, shape (n_train_samples,)
+        Indices of training samples.
+
+    test : array-like, shape (n_test_samples,)
+        Indices of test samples.
+
+    verbose : integer
+        The verbosity level.
+
+    error_score : 'raise' (default) or numeric
+        Value to assign to the score if an error occurs in estimator fitting.
+        If set to 'raise', the error is raised. If a numeric value is given,
+        FitFailedWarning is raised. This parameter does not affect the refit
+        step, which will always raise the error.
+
+    parameters : dict or None
+        Parameters to be set on the estimator.
+
+    fit_params : dict or None
+        Parameters that will be passed to ``estimator.fit``.
+
+    return_train_score : boolean, optional, default: False
+        Compute and return score on training set.
+
+    return_parameters : boolean, optional, default: False
+        Return parameters that has been used for the estimator.
+
+    return_n_test_samples : boolean, optional, default: False
+        Whether to return the ``n_test_samples``
+
+    return_times : boolean, optional, default: False
+        Whether to return the fit/score times.
+
+    Returns
+    -------
+    train_scores : dict of scorer name -> float, optional
+        Score on training set (for all the scorers),
+        returned only if `return_train_score` is `True`.
+
+    test_scores : dict of scorer name -> float, optional
+        Score on testing set (for all the scorers).
+
+    n_test_samples : int
+        Number of test samples.
+
+    fit_time : float
+        Time spent for fitting in seconds.
+
+    score_time : float
+        Time spent for scoring in seconds.
+
+    parameters : dict or None, optional
+        The parameters that have been evaluated.
+
+    """
+    if verbose > 1:
+        if parameters is None:
+            msg = ''
+        else:
+            msg = '%s' % (
+                ', '.join('%s=%s' % (k, v) for k, v in parameters.items()))
+        print("[CV] %s %s" % (msg, (64 - len(msg)) * '.'))
+
+    # Adjust length of sample weights
+    fit_params = fit_params if fit_params is not None else {}
+    fit_params = dict(
+        [(k, _index_param_value(X, v, train)) for k, v in fit_params.items()])
+
+    test_scores = {}
+    train_scores = {}
+    if parameters is not None:
+        estimator.set_params(**parameters)
+
+    start_time = time.time()
+
+    # do it for each patient
+    X_train, y_train, X_test, y_test = _safe_split_multi(
+        estimator, X, y, train, test)
+
+    is_multimetric = not callable(scorer)
+    n_scorers = len(scorer.keys()) if is_multimetric else 1
+
+    try:
+        if y_train is None:
+            estimator.fit(X_train, **fit_params)
+        else:
+            estimator.fit(X_train, y_train, **fit_params)
+
+    except Exception as e:
+        # Note fit time as time until error
+        fit_time = time.time() - start_time
+        score_time = 0.0
+        if error_score == 'raise':
+            raise
+        elif isinstance(error_score, numbers.Number):
+            if is_multimetric:
+                test_scores = dict(
+                    zip(scorer.keys(), [
+                        error_score,
+                    ] * n_scorers))
+                if return_train_score:
+                    train_scores = dict(
+                        zip(scorer.keys(), [
+                            error_score,
+                        ] * n_scorers))
+            else:
+                test_scores = error_score
+                if return_train_score:
+                    train_scores = error_score
+            warnings.warn(
+                "Classifier fit failed. The score on this train-test"
+                " partition for these parameters will be set to %f. "
+                "Details: \n%r" % (error_score, e), FitFailedWarning)
+        else:
+            raise ValueError(
+                "error_score must be the string 'raise' or a"
+                " numeric value. (Hint: if using 'raise', please"
+                " make sure that it has been spelled correctly.)")
+
+    else:
+        fit_time = time.time() - start_time
+        # _score will return dict if is_multimetric is True
+        test_scores = _score(estimator, X_test, y_test, scorer, is_multimetric)
+        score_time = time.time() - start_time - fit_time
+        if return_train_score:
+            train_scores = _score(
+                estimator, X_train, y_train, scorer, is_multimetric)
+
+    if verbose > 2:
+        if is_multimetric:
+            for scorer_name, score in test_scores.items():
+                msg += ", %s=%s" % (scorer_name, score)
+        else:
+            msg += ", score=%s" % test_scores
+    if verbose > 1:
+        total_time = score_time + fit_time
+        end_msg = "%s, total=%s" % (msg, logger.short_format_time(total_time))
+        print("[CV] %s %s" % ((64 - len(end_msg)) * '.', end_msg))
+
+    ret = [train_scores, test_scores] if return_train_score else [test_scores]
+
+    if return_n_test_samples:
+        ret.append(_num_samples(X_test))
+    if return_times:
+        ret.extend([fit_time, score_time])
+    if return_parameters:
+        ret.append(parameters)
+    if return_estimator:
+        ret.append(estimator)
+    if return_idx:
+        ret.extend([train, test])
+    return ret
+
+
 class MultipleKernelGridSearchCV(GridSearchCV):
     # Ensure consistent split
     _pairwise = True
@@ -538,8 +538,8 @@ class MultipleKernelGridSearchCV(GridSearchCV):
             n_jobs=self.n_jobs, verbose=self.verbose,
             pre_dispatch=pre_dispatch)(
                 delayed(_fit_and_score)(
-                    clone(base_estimator), X, y, scorers, train, test, self.
-                    verbose, parameters, fit_params=fit_params,
+                    clone(base_estimator), X, y, scorers, train, test,
+                    self.verbose, parameters, fit_params=fit_params,
                     return_train_score=self.return_train_score,
                     return_n_test_samples=True, return_times=True,
                     return_parameters=False, error_score=self.error_score,
